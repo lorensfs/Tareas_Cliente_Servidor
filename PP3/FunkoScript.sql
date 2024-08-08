fa1c323586b90cde635d68f0c0cc9ab7862f4755
Lorenzo FS <lorenzofs1000@gmail.com>
2024-08-08 14:36:36 -0600

    update script

--- PP3/FunkoScript.sql
+++ PP3/FunkoScript.sql
@@ -1,28 +1,36 @@
 CREATE DATABASE figures_funko;
 
-CREATE TABLE figures_funko.funkos(
-	funko_name VARCHAR(20) NOT NULL PRIMARY KEY, 
-    description VARCHAR(100) NOT NULL,
-    vaulted boolean DEFAULT FALSE NOT NULL
-);
+CREATE TABLE
+    figures_funko.funkos (
+        funko_name VARCHAR(20) NOT NULL PRIMARY KEY,
+        description VARCHAR(100) NOT NULL,
+        vaulted boolean DEFAULT FALSE NOT NULL
+    );
 
-INSERT INTO figures_funko.funkos(funko_name, description, vaulted)
-VALUE 
-	('Superman', 'Superman action figure', FALSE),
+INSERT INTO
+    figures_funko.funkos (funko_name, description, vaulted)
+VALUES
+    ('Superman', 'Superman action figure', FALSE),
     ('Batman', 'Batman action figure', FALSE),
-    ('WonderWoman', 'Wonder Woman action figure', FALSE),
+    (
+        'WonderWoman',
+        'Wonder Woman action figure',
+        FALSE
+    ),
     ('SpiderMan', 'Spider-Man action figure', FALSE),
     ('IronMan', 'Iron Man action figure', FALSE);
 
 -- Let's say we gotta put one at vault (IronMan)
 UPDATE figures_funko.funkos
-SET vaulted = TRUE
-WHERE funko_name = 'IronMan';
-    
--- Verify data
-SELECT *
-FROM figures_funko.funkos
-ORDER BY funko_name;
-    
-	
+SET
+    vaulted = TRUE
+WHERE
+    funko_name = 'IronMan';
 
+-- Verify data
+SELECT
+    *
+FROM
+    figures_funko.funkos
+ORDER BY
+    funko_name;
